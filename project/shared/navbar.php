cf400af515de6618bbb7027972ce5af94a5e280e
Brandon <brandonneuta14@gmail.com>
2024-04-01 10:36:20 -0500

    ajustes rutas

--- project/shared/navbar.php
+++ project/shared/navbar.php
@@ -5,9 +5,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item"><a class="nav-link active" aria-current="page"  href="/haircare-mall/project/index.php">Inicio</a></li>
-                        <li class="nav-item"><a class="nav-link" href="catalogo/catalogo.php">Catálogo</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/haircare-mall/project/catalogo/catalogo.php">Catálogo</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">Contactanos</a></li>
-                        <li class="nav-item"><a class="nav-link" href="login/InicioSesion.php">Iniciar sesión</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/haircare-mall/project/login/InicioSesion.php">Iniciar sesión</a></li>
                     </ul>
                 </div>
             </div>
