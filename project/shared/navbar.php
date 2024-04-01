de48535379a187f448f386c2edc75ba3015e9970
Brandon <brandonneuta14@gmail.com>
2024-04-01 12:38:43 -0500

    Ajuste barra

--- project/shared/navbar.php
+++ project/shared/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg " style="">
             <div class="container px-5">
-                <a class="navbar-brand"  href="#!"><img src="/haircare-mall/project/img/logo_system.png" style="max-width:70px;" alt=""></a>
+                <a class="navbar-brand"  href="/haircare-mall/project/index.php"><img src="/haircare-mall/project/img/logo_system.png" style="max-width:70px;" alt=""></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
