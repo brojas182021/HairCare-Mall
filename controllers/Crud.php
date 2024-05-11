5b9bd5f2e40ed0c2c1700223b4da4a8b6efa4d64
emilysm14 <108769362+emilysm14@users.noreply.github.com>
2024-05-10 21:56:34 -0500

    Add files via upload
    Crud actualizar

--- controllers/Crud.php
+++ controllers/Crud.php
@@ -52,5 +52,55 @@ class Crud extends Controller{
         }
         
      }
+     function verProducto($param= null){
+        $idProducto = $param[0];
+        $producto = $this->model->getById($idProducto);
+
+        session_start();
+        $_SESSION['id_verProducto'] = $producto->prod_codigo;
+        $this->view->producto = $producto;
+        $this->view->mensaje= "";
+        $this->view->render('crud/update/detalleProd');
+
+     }
+
+     function actualizarProducto($param= null){
+       
+        $prod_codigo  = $_POST['prod_codigo'];
+        $prod_nombre  = $_POST['prod_nombre'];
+        $prod_precio  = $_POST['prod_precio'];
+        $prod_stock   = $_POST['prod_stock'];
+        $prod_descrip = $_POST['prod_descrip'];
+        unset($_SESSION['id_verProducto']);
+       
+    //  if($this->model->update(['prod_codigo' => $prod_codigo, 'prod_nombre' => $prod_nombre, 'prod_precioVenta' => $prod_precio, 'prod_Stock' => $prod_stock, 'prod_descripcion' => $prod_descrip])){
+            
+    //         $producto = new Product();
+    //         $producto -> prod_codigo = $prod_codigo;
+    //         $producto -> prod_nombre = $prod_nombre;
+    //         $producto -> prod_precio = $prod_precio;
+    //         $producto -> prod_stock = $prod_stock;
+    //         $producto -> prod_descrip = $prod_descrip;
+    //         $this->view->producto = $producto;
+    //         $this->view->mensaje = "Producto Actualizado correctamente";
+    //     }else{
+    //         $this->view->mensaje = "Producto no se pudo actualizar";
+    //     }
+    //     $this->render('crud/detalleProd');
+
+
+        $resultado =$this->model->update(['prod_codigo' => $prod_codigo, 'prod_nombre' => $prod_nombre, 'prod_precioVenta' => $prod_precio, 'prod_Stock' => $prod_stock, 'prod_descripcion' => $prod_descrip]);
+        if ($resultado) {
+            // Mostrar un alert en JavaScript indicando que la inserción fue exitosa
+            echo '<script>alert("'.$resultado.'");</script>';
+            //echo "<script>window.location.href = '" . constant('URL') . "crud';</script>";
+            $this->render();
+        } else {
+            // Mostrar un alert en JavaScript con el mensaje de error específico
+            echo '<script>alert("'.$resultado.'");</script>';
+            //echo "<script>window.location.href = '" . constant('URL') . "crud';</script>";
+            $this->render();
+        }
+     }
 }
 ?>
